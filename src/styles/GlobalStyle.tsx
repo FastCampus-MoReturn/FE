ce35aae99aee175207fecdb032d3795c486b7350
0nesan <0nesan.o8o@gmail.com>
2023-04-01 16:09:54 +0900

    Feat: 토스트 모달 스타일링, 옵션 추가
    * auth.ts : 모달 옵션 추가하였습니다.
    * main.ts : 모달 컴포넌트에 메세지 중복 방지를 위해 limit props를
      넣었습니다.
    * GlobalStyled.tsx : 컴포넌트 스타일링 위해 스타일 작성하여 내보내기
      했습니다.

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { css, Global } from '@emotion/react';
 import reset from 'emotion-reset';
+import styled from '@emotion/styled';
+import { ToastContainer } from 'react-toastify';
 import COLORS from './colors';
 
 const style = css`
@@ -35,5 +37,53 @@ const style = css`
 const GlobalStyle = () => {
   return <Global styles={style} />;
 };
+
+// tostmodal custom css
+export const Container = styled(ToastContainer)`
+  .Toastify__toast {
+    font-size: 16px;
+    border-radius: 50px;
+    padding: 16px 28px;
+    border-radius: 16px;
+    font-size: 20px;
+    font-weight: 600;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  --toastify-toast-width: 1030px;
+
+  .Toastify__toast-icon {
+    width: 0;
+    height: 0;
+    display: none;
+    position: absolute;
+    top: -1px;
+    left: -1px;
+    margin: -1px;
+  }
+
+  .Toastify__toast--info {
+    background-color: #fff;
+    border: 1px solid #ddd;
+  }
+
+  .Toastify__toast--success {
+    background-color: #fff;
+    border: 1px solid #ddd;
+  }
+
+  .Toastify__toast--error {
+    background-color: #fff;
+    border: 1px solid #ff5252;
+    color: #ff5252;
+
+    div {
+      text-align: center;
+    }
+  }
+`;
 
 export default GlobalStyle;
