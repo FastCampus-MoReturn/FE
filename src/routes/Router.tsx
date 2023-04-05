52b2370aea220c2d9c32867c898c105449ad047f
0nesan <0nesan.o8o@gmail.com>
2023-04-05 22:11:35 +0900

    Feat: 용어사전 사전 마크업 작업

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,11 +4,13 @@ import PdfSend from '@/pages/PdfSend';
 import PDFComp from '@/components/pdf-send/PDFComp';
 import InfoCheckPage from '@/pages/InfoCheckPage';
 import PdfCommentary2 from '@/pages/PdfCommentary2';
+import Dictionary from '@/pages/Dictionary';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="infocheck" element={<InfoCheckPage />} />
+      <Route path="dictionary" element={<Dictionary />} />
       <Route path="pdf-send" element={<PdfSend />} />
       <Route path="pdf" element={<PDFComp />} />
       <Route path="commentary" element={<PdfCommentary2 />} />
